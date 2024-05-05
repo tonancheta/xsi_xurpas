6d6cb38545c0c0939256bfd5c8f242069c80c944
Ton Ancheta <ton@xurpas.com>
2024-05-05 11:32:03 +0800

    2024-05-04 WIP #2

--- xurpas/xurpas_customizations/web_form/partner_orders/partner_orders.js
+++ xurpas/xurpas_customizations/web_form/partner_orders/partner_orders.js
@@ -1,3 +1,4 @@
-frappe.ready(function() {
-	// bind events here
-})
+frappe.web_form.after_load = () => {
+	// init script here
+	frappe.web_form.set_value('delivery_date', add_to_date(frappe.datetime.nowdate(), days = 10))
+}
