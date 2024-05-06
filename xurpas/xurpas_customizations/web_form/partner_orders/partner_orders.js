44d67ac3b631dc66fb39c3ce843cfa4ea2c814f4
tonancheta <trancheta@gmail.com>
2024-05-06 12:59:07 +0800

    2024-05-06 WIP #1

--- xurpas/xurpas_customizations/web_form/partner_orders/partner_orders.js
+++ xurpas/xurpas_customizations/web_form/partner_orders/partner_orders.js
@@ -1,5 +1,5 @@
-frappe.web_form.after_load = () => {
-	// init script here
-	console.log('web form loaded!')
-	frappe.web_form.set_value('delivery_date', add_to_date(frappe.datetime.nowdate(), days = 10))
-}
+// frappe.web_form.after_load = () => {
+// 	// init script here
+// 	console.log('web form loaded!')
+// 	frappe.web_form.set_value('delivery_date', add_to_date(frappe.datetime.nowdate(), days = 10))
+// }
